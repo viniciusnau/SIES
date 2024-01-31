9069ba7816098591f6b4055a6e358ab6c1ca3ec8
Guilherme Pereira <guilhermepereira@defensoria.sc.gov.br>
2024-01-31 15:02:57 -0300

    Fix: register page select

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -23,7 +23,7 @@ const Register = () => {
   const [snackbarType, setSnackbarType] = useState<boolean>(false);
   const [form, setForm] = useState<iForm>({
     name: "",
-    public_defense: "",
+    public_defense: public_defenses[0].property,
     birth_date: "",
     category: "",
     is_pcd: false,
@@ -52,9 +52,9 @@ const Register = () => {
       };
     }
   };
-
   const handleSubmit = () => {
     handleFormat();
+    console.log("form: ", form);
     dispatch(fetchPostRegister(formatted));
     setSnackbarType(true);
   };
