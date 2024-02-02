cc1e3131dea214fc6382095d0cac5c42b7947340
Vinicius Nau <viniciuscoelhonau975@gmail.com>
2024-02-02 13:57:36 -0300

    chore: Stage Text

--- src/Pages/Rank/Rank.tsx
+++ src/Pages/Rank/Rank.tsx
@@ -133,7 +133,7 @@ const Rank: React.FC = () => {
                   }`}
                 >
                   Etapa 1: somente Índice de Mérito Acadêmico Acumulado (IMAA),
-                  Etapa 2: média entre IMAA, entrevista e prova
+                  Etapa 2: média entre IMAA, entrevista e/ou prova
                 </p>
               )}
               <BsQuestionSquare
