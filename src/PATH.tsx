3eed29ecd7ee6ba30b08c2904ab2dd86c6435a33
Vinicius Nau <viniciuscoelhonau975@gmail.com>
2024-03-06 16:12:00 -0300

    merge: Merge From Developer

--- src/PATH.tsx
+++ src/PATH.tsx
@@ -1,6 +1,6 @@
-// export const PATH = {
-//   base: "https://sies.defensoria.sc.def.br/api",
-// };
 export const PATH = {
-  base: "https://dev-sies.defensoria.sc.def.br/api",
+  base: "https://sies.defensoria.sc.def.br/api",
 };
+// export const PATH = {
+//   base: "https://dev-sies.defensoria.sc.def.br/api",
+// };
